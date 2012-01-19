5818e570eef4bcca6d674da921f0f0672dc39359
Alex Leigh <dualityim@gmail.com>
2012-01-19 03:17:25 -0500

    Revised init sequence

--- rootdir/init.sh
+++ rootdir/init.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 
-if [ -f /system/usr/bin/busybox ] && [ -d /system/usr/etc/init.d ]; then
-    /system/bin/logwrapper /system/usr/bin/busybox run-parts /system/usr/etc/init.d;
+if [ -f /system/xbin/busybox ] && [ -d /system/etc/init.d ]; then
+    /system/bin/logwrapper /system/xbin/busybox run-parts /system/etc/init.d;
 fi
 
 sync;
-setprop sys.runinit.complete 1;
+setprop sys.setup.complete 1;
 
